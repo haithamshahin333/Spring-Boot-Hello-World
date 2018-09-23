3f76a6ae716ace0195aa8a196a438955eb6d6f5c
haithamshahin333 <haithamshahin3@gmail.com>
2018-09-23 17:06:29 -0400

    New hello world spring boot app

---
+++ src/main/java/test/example/hello/world/helloWorldTest/HelloWorldController.java
@@ -0,0 +1,14 @@
+package test.example.hello.world.helloWorldTest;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello World";
+	}
+	
+}
---
+++ src/main/java/test/example/hello/world/helloWorldTest/HelloWorldTestApplication.java
@@ -0,0 +1,12 @@
+package test.example.hello.world.helloWorldTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldTestApplication.class, args);
+	}
+}
